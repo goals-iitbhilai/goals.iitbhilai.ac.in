4afc102a58935470ff303732e7052f1be2b8726c
AryaveerSR <me.aryaveer@gmail.com>
2026-05-02 16:48:32 +0530

    add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      fallbacks: ["sans-serif"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Crimson Pro",
+      cssVariable: "--font-crimson",
+      fallbacks: ["serif"],
+    },
+  ],
 });
