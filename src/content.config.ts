95e8b0e59e396dc6312874b2da371ecd97319989
AryaveerSR <me.aryaveer@gmail.com>
2026-05-12 11:29:07 +0530

    move links and socials into content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,9 @@
 import { defineCollection } from "astro:content";
-import { glob } from "astro/loaders";
+import { glob, file } from "astro/loaders";
 import { z } from "zod";
+
+const parser = (json: string) =>
+  JSON.parse(json).map((item: any, index: number) => ({ ...item, id: index }));
 
 const alumni = defineCollection({
   loader: glob({ pattern: "./*/data.json", base: "./content/alumni" }),
@@ -13,4 +16,14 @@ const alumni = defineCollection({
     }),
 });
 
-export const collections = { alumni };
+const links = defineCollection({
+  loader: file("./content/links.json", { parser }),
+  schema: z.object({ name: z.string(), href: z.string() }),
+});
+
+const socials = defineCollection({
+  loader: file("./content/socials.json", { parser }),
+  schema: z.object({ name: z.string(), icon: z.string(), href: z.string() }),
+});
+
+export const collections = { alumni, links, socials };
