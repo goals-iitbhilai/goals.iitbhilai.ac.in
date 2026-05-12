a814f0954db807ef7aae4d747b11c0902caba93b
AryaveerSR <me.aryaveer@gmail.com>
2026-05-12 11:12:25 +0530

    footer design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/utils/socials.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: "LinkedIn",
+    icon: "mdi:linkedin",
+    href: "https://www.linkedin.com/company/general-oratory-and-literary-society-goals/",
+  },
+  {
+    name: "Facebook",
+    icon: "mdi:facebook",
+    href: "https://www.facebook.com/profile.php?id=61580617536916",
+  },
+  {
+    name: "Instagram",
+    icon: "mdi:instagram",
+    href: "https://www.instagram.com/goals_iitbhilai/",
+  },
+];
