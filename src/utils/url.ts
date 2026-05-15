8ffa59afd9e031b18fc0536a999bb355020d629b
AryaveerSR <me.aryaveer@gmail.com>
2026-05-15 20:52:27 +0530

    add seo tags

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,10 @@
 export const homeURL = import.meta.env.BASE_URL;
+export const absoluteURL = import.meta.env.SITE + import.meta.env.BASE_URL;
 
 export function getURL(path: string): string {
-  return `${import.meta.env.BASE_URL}${path.startsWith("/") ? path.substring(1) : path}`;
+  return `${homeURL}${path.startsWith("/") ? path.substring(1) : path}`;
+}
+
+export function getAbsoluteURL(path: string): string {
+  return `${absoluteURL}${path.startsWith("/") ? path.substring(1) : path}`;
 }
