1e52492420f9ea347cdbc41de78fb4d538e90d23
aryaveersr <me.aryaveer@gmail.com>
2026-07-14 17:17:28 +0530

    rewrite urls for github pages using an integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,9 @@ export default defineConfig({
   site: isPages
     ? "https://goals-iitbhilai.github.io"
     : "https://goals.iitbhilai.ac.in",
-  base: isPages ? "/goals.iitbhilai.ac.in/" : "/",
+  base: isPages ? "/goals.iitbhilai.ac.in" : "/",
+  integrations: [icon(), isPages && rewritePaths()],
 
-  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
     server: {
@@ -46,3 +46,32 @@ export default defineConfig({
     }),
   },
 });
+
+/** @returns {import("astro").AstroIntegration} */
+function rewritePaths() {
+  return {
+    name: "rewrite-paths",
+    hooks: {
+      "astro:build:done": async ({ dir }) => {
+        const { globby } = await import("globby");
+        const fs = await import("node:fs/promises");
+
+        const files = await globby("**/*.html", {
+          cwd: dir.pathname,
+          absolute: true,
+        });
+
+        await Promise.all(
+          files.map(async (file) => {
+            let html = await fs.readFile(file, "utf-8");
+            html = html.replace(
+              /(href|src|action)="\/(?!\/|goals\.iitbhilai\.ac\.in)/g,
+              `$1="/goals.iitbhilai.ac.in/`,
+            );
+            await fs.writeFile(file, html);
+          }),
+        );
+      },
+    },
+  };
+}
