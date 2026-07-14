e7d1d35a42eade15281dc693e884956a250808c6
aryaveersr <me.aryaveer@gmail.com>
2026-07-14 19:47:18 +0530

    cleanup content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,9 @@
 import { defineCollection } from "astro:content";
 import { glob, file } from "astro/loaders";
-import { describe, z } from "zod";
-import { title } from "node:process";
-
-function parser<T>(json: string) {
-  return JSON.parse(json).map((item: T, index: number) => ({
-    ...item,
-    id: index,
-  }));
-}
+import { z } from "zod";
 
 const alumni = defineCollection({
-  loader: glob({ pattern: "./*/data.json", base: "./content/alumni" }),
+  loader: glob({ pattern: "./content/alumni/*/data.json" }),
   schema: ({ image }) =>
     z.object({
       name: z.string(),
@@ -22,7 +14,7 @@ const alumni = defineCollection({
 });
 
 const team = defineCollection({
-  loader: glob({ pattern: "./*/data.json", base: "./content/team" }),
+  loader: glob({ pattern: "./content/team/*/data.json" }),
   schema: ({ image }) =>
     z.object({
       name: z.string(),
@@ -33,14 +25,13 @@ const team = defineCollection({
 });
 
 const links = defineCollection({
-  loader: file("./content/links.json", { parser }),
-  schema: z.object({ name: z.string(), href: z.string() }),
+  loader: file("./content/links.json"),
+  schema: z.string(),
 });
 
 const socials = defineCollection({
-  loader: file("./content/socials.json", { parser }),
+  loader: file("./content/socials.json"),
   schema: z.object({
-    name: z.string(),
     icon: z.string(),
     theme: z.string(),
     href: z.string(),
