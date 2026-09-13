4c0b1d4b72b678b3204d77a6355fa7ce1669b7cc
aryaveersr <me.aryaveer@gmail.com>
2026-09-13 14:58:57 +0530

    make stripping trailing slash during normalization explicit

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -3,5 +3,6 @@ import path from "node:path";
 export function normalize(url: string) {
   url = url.replace(import.meta.env.BASE_URL, "/");
   url = path.normalize(url);
-  return path.resolve(url);
+
+  return url.length > 1 ? url.replace(/\/+$/, "") : url;
 }
