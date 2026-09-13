6d56b651de8afebd16d167aac7a629c43471023c
aryaveersr <me.aryaveer@gmail.com>
2026-09-13 14:37:11 +0530

    chore: simplify deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Guard against .env file not existing
 if [ ! -f .env ]; then
-    echo "Error: .env file not found. See .env.example for instructions."
+    echo "Error: .env file not found."
     exit 1
 fi
 
@@ -10,7 +10,7 @@ fi
 export $(cat .env | xargs)
 
 # Guard against environment variables not existing
-if [[ -z "$GOALS_SSH_HOSTNAME" || -z "$GOALS_SSH_USERNAME" || -z "$GOALS_SSH_PASSWORD" ]]; then
+if [[ -z "$SSH_HOST" || -z "$SSH_USER" || -z "$SSH_PASS" || -z "$SSH_DEST" ]]; then
     echo "Error: One or more required environment variables are missing."
     exit 1
 fi
@@ -21,42 +21,10 @@ if ! command -v sshpass &> /dev/null; then
     exit 1
 fi
 
-# Build site
-echo "Building site..."
+# Build the site using Astro.
 npm run build
 
-# Returns status of Cloudflare warp
-get_warp_cli_status() {
-    warp-cli status | grep "Status update" | awk '{print $3}'
-}
-
-# Copies the built site to the server
-copy_site() {
-    # If warp is installed, store its current state and disable it
-    local was_warp_enabled
-    if command -v warp-cli &> /dev/null; then
-        was_warp_enabled=$(get_warp_cli_status)
-        echo "warp-cli status: $was_warp_enabled"
-
-        if [ "$was_warp_enabled" = "Connected" ]; then
-            warp-cli disconnect
-            sleep 1
-        fi
-    fi
-
-    # Copy the files using rsync
-    export SSHPASS="$GOALS_SSH_PASSWORD"
-    sshpass -e rsync -avz "dist/" "$GOALS_SSH_USERNAME@$GOALS_SSH_HOSTNAME:/srv/goals.iitbhilai.ac.in/pub"
-
-    # If warp is installed, restore its original state
-    if command -v warp-cli &> /dev/null; then
-        if [ "$was_warp_enabled" = "Connected" ]; then
-            warp-cli connect
-        fi
-    fi
-}
-
-# Start copying built assets
-echo "Copying site..."
-copy_site
-echo "Done."
+# Use `sshpass` and `rsync` to sync the built assets.
+SSHPASS="$SSH_PASS" sshpass \
+    -e rsync -avzP --delete \
+    "dist/" "$SSH_USER@$SSH_HOST:$SSH_DEST"
