2bdaba1e2b0c7504e4f41efeebfd0936d4cfb531
aryaveersr <me.aryaveer@gmail.com>
2026-09-14 16:48:56 +0530

    eslint config for svelte

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,21 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginSvelte from "eslint-plugin-svelte";
 import tseslint from "typescript-eslint";
+import svelteParser from "svelte-eslint-parser";
 
 export default [
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginSvelte.configs.recommended,
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  },
   {
     rules: {},
   },
