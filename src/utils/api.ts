3d0106c7b23235b3e39678e48887af7047863ee4
aryaveersr <me.aryaveer@gmail.com>
2026-09-14 15:41:59 +0530

    merge utils

--- src/utils/api.ts
+++
@@ -1,3 +0,0 @@
-export function api(path: string): string {
-  return `https://backend-five-delta-42.vercel.app/api${path}`;
-}
--- src/utils/path.ts
+++
@@ -1,8 +0,0 @@
-import path from "node:path";
-
-export function normalize(url: string) {
-  url = url.replace(import.meta.env.BASE_URL, "/");
-  url = path.normalize(url);
-
-  return url.length > 1 ? url.replace(/\/+$/, "") : url;
-}
